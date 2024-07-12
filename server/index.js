b803d1ceac019fb302091b4309cf66621b623ba8
Khaja Qutbuddin <khajaqutbuddin123@gmail.com>
2024-07-12 22:02:36 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,7 @@
+// This is a Starting Point of our server application's
+
+import express from "express"
+import cors from "cors" 
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+
